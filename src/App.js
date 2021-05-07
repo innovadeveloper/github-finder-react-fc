cace13387b3a8c8dc5ccddc7cf138146cc8a71bd
kane baltazar alanoca <developer.innovaappstar@gmail.com>
2021-05-07 18:49:05 -0500

    Search component added with function passed between child and parent

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar"
 import "./App.css";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 
 import axios from "axios";
 
@@ -21,12 +22,20 @@ class App extends Component {
     this.setState({loading : false, users : rest.data})
   }
 
+  // Search Github users
+  searchUsers = async text => {
+    this.setState({loading : true})
+    const rest = await axios.get(`https://api.github.com/search/users?q=${text}`)
+    this.setState({loading : false, users : rest.data.items})
+  }
+
   render() {
     return (
       <React.Fragment>
         <div className="App">
           <Navbar title="Github Finder" icon="fab fa-github"/>
           <div className="container">
+            <Search searchUsers={this.searchUsers}/>
             <Users loading={this.state.loading} users={this.state.users}/>
           </div>
         </div>
