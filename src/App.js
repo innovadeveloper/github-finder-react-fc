cc458a51ca23bd343e20cfd605782e21cdf9ba52
kane baltazar alanoca <developer.innovaappstar@gmail.com>
2021-05-07 23:28:22 -0500

    Router and main route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar"
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
@@ -9,53 +10,69 @@ import axios from "axios";
 
 // rce -> class component
 class App extends Component {
-
   state = {
-    loading : false,
-    users : [],
-    alert : null
-  }
+    loading: false,
+    users: [],
+    alert: null,
+  };
 
-  async componentDidMount(){
+  async componentDidMount() {
     // console.log(process.env.REACT_APP_CLIENT_ID);
-    this.setState({loading : true})
-    const rest = await axios.get("https://api.github.com/users")
+    this.setState({ loading: true });
+    const rest = await axios.get("https://api.github.com/users");
 
-    this.setState({loading : false, users : rest.data})
+    this.setState({ loading: false, users: rest.data });
   }
 
   // Search Github users
-  searchUsers = async text => {
-    this.setState({loading : true})
-    const rest = await axios.get(`https://api.github.com/search/users?q=${text}`)
-    this.setState({loading : false, users : rest.data.items})
-  }
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const rest = await axios.get(
+      `https://api.github.com/search/users?q=${text}`
+    );
+    this.setState({ loading: false, users: rest.data.items });
+  };
 
   // clear Github users
-  clearUsers = () =>{
-    this.setState({users : [], loading : false});
-  }
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
 
   setAlert = (msg, type) => {
-    this.setState({alert : {msg, type}});
+    this.setState({ alert: { msg, type } });
     setTimeout(() => {
-      this.setState({alert : null});
+      this.setState({ alert: null });
     }, 3000);
-  }
+  };
 
   render() {
-    const {users, loading} = this.state
+    const { users, loading } = this.state;
     return (
-      <React.Fragment>
+      <Router>
         <div className="App">
-          <Navbar title="Github Finder" icon="fab fa-github"/>
+          <Navbar title="Github Finder" icon="fab fa-github" />
           <div className="container">
-            <Alert alert={this.state.alert}/>
-            <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0} setAlert={this.setAlert}/>
-            <Users loading={loading} users={users}/>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+            </Switch>
           </div>
         </div>
-      </React.Fragment>
+      </Router>
     );
   }
 }
